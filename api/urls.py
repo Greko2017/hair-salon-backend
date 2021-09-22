ddbc14f9492e32d87ca59f6f17427b7a159623c1
Goufan A Zock, Gregory <33413355+Greko2017@users.noreply.github.com>
2021-09-22 11:15:36 +0100

    added static librairi in api.urls

--- api/urls.py
+++ api/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf.urls.static import static
 from django.conf import settings
 from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework_simplejwt.views import TokenObtainPairView
