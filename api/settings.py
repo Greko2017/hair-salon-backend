285f865d429df9ef66ac7fb77ae3b75e8f139d15
Goufan A Zock, Gregory <33413355+Greko2017@users.noreply.github.com>
2021-09-22 09:47:34 +0100

    modify BASE_DIR

--- api/settings.py
+++ api/settings.py
@@ -14,10 +14,10 @@ import os
 import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-BASE_DIR = os.path.dirname(os.path.dirname(
-    os.path.dirname(os.path.abspath(__file__))))
+# BASE_DIR = os.path.dirname(os.path.dirname(
+#     os.path.dirname(os.path.abspath(__file__))))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
