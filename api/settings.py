a3e8115117af1de0ed466004deafb1d39964a97b
Goufan A Zock, Gregory <33413355+Greko2017@users.noreply.github.com>
2021-10-03 21:39:44 +0100

    ft

--- api/settings.py
+++ api/settings.py
@@ -195,7 +195,7 @@ CORS_ORIGIN_WHITELIST = [
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'gregory.goufan@gmail.com' #os.environ.get('EMAIL_ADDRESS')
