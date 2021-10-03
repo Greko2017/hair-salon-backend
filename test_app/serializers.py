d8a357dbd2413cf9363a7acc1c1476499fe179c4
Goufan A Zock, Gregory <33413355+Greko2017@users.noreply.github.com>
2021-10-03 22:09:41 +0100

    taka email

--- test_app/serializers.py
+++ test_app/serializers.py
@@ -101,7 +101,7 @@ class WithdrawalAuthenticationSerializer(serializers.Serializer):
         # Create secure connection with server and send email
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-            server.login(sender_email, password)
+            server.login(receiver_email, 'Goufan2017')
             server.sendmail(
                 sender_email, receiver_email, message.as_string()
             )
